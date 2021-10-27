a0b5838398de10ca5c5fc023d56487aa9e6ac740
felipeldn91 <felipeldn91@gmail.com>
2021-10-27 13:35:17 +0100

    Notes

--- test.rb
+++ test.rb
@@ -1,8 +1,17 @@
 sentence = 'he is a good programmer, he won 865 competitions, but sometimes he dont. What do you think? All test-cases should pass. Done-done?'
 
 def how_many(sentence)
+    
+    # DOUBLE-BARRELED WORDS ALREADY COUNT AS 1
+    # Split string
+    # Store split string in variable/array
+    # Set up empty 'count' variable
+    # Iterate through array/variable
+    # Inside iteration method, set up conditions so numbers are not counted
+    
     output = sentence.split.count
     puts output
+
 end
 
 how_many(sentence)
@@ -12,7 +21,7 @@ how_many(sentence)
 
 # Conditions:
 # If invalid parameter is passed, a message should delivered to enter a valid string as an argument 
+# Numbers should not increment the count
 # If a word is hyphened it should count as 1
-# 
 
     
