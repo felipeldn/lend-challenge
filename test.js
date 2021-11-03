f607fdc5d9eeae9fa5dcd53aa3d3a06bb3c508a8
felipeldn91 <felipeldn91@gmail.com>
2021-11-03 19:30:41 +0000

    testing with JS

---
+++ test.js
@@ -0,0 +1,34 @@
+let sentence = 'b? Dl )B 4(V! A. MK, YtG ](f 1m )CNxuNUR {PG?'
+let test_sentence = 34
+
+function how_Many(sentence) {
+
+    if (typeof sentence === 'string') {
+        
+        let output = sentence.split()
+        let count = 0
+
+        let test_output = []
+
+        output.forEach(function(word) {
+
+            let hasNumber = /\d/
+            let hasSpecialCharacter = /[!@#$%^&*()_+{}\[\]:;'"\/\\><]/
+
+            if (hasNumber.test(word) ===! true ) {
+                count
+            } else {
+                test_output.push(word)
+                count += 1
+            }
+            console.log(test_output)
+            console.log(count)
+        })
+
+    } else {
+        console.log('Please enter a valid a string')
+    }
+
+}
+
+how_Many(sentence)
